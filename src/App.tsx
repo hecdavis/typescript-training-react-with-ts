918b064b296704372ab6adc748e0c0daf140b4e3
Hector Davis <hector.davis94@gmail.com>
2021-07-23 21:31:10 -0300

    project finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return (
+    <div className='App'>
+      <NewTodo
+        updateTodos={(newTodo: string) =>
+          setTodos((prevState) => [
+            ...prevState,
+            { id: Math.random().toString(), text: newTodo },
+          ])
+        }
+      />
+      <TodoList
+        items={todos}
+        onDelete={(id: string) =>
+          setTodos((prevState) => prevState.filter((todo) => todo.id !== id))
+        }
+      />
+    </div>
+  );
 };
 
 export default App;
